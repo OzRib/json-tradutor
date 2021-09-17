76f174e8485b87b6b4eaa65687d2b164bcb09ea7
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-09-17 16:26:36 -0300

    Opening browser in google translate

--- translate.py
+++ translate.py
@@ -4,3 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 ARGS = sys.argv
 to_translate = ARGS[1]
+
+path = ChromeDriverManager().install()
+browser = webdriver.Chrome(executable_path=path)
+browser.get('https://translate.google.pt')
