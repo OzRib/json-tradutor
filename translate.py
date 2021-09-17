2d5a1980d2ee058b2eeabcd7a7111e02c05615d5
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-09-17 16:30:37 -0300

    Putting work to translate in input textarea

--- translate.py
+++ translate.py
@@ -8,3 +8,9 @@ to_translate = ARGS[1]
 path = ChromeDriverManager().install()
 browser = webdriver.Chrome(executable_path=path)
 browser.get('https://translate.google.pt')
+
+# er8xn is the input textarea
+input_field = browser.find_element_by_class_name('er8xn')
+
+# putting work to translate in input textarea
+input_field.send_keys(to_translate)
