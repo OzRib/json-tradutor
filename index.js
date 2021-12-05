243be742577d46e662a249786ec625905bd1e888
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-12-05 17:50:47 -0300

    Adding function get json

--- index.js
+++ index.js
@@ -15,6 +15,50 @@ function execShellCommand(cmd){
   });
 }
 
+function getJson(){
+  function getArgsProperties(){
+    const args = process.argv.slice(2);
+    const jsonFilePath = args[0];
+    const exportName = args[1];
+
+    function formatJsonFilePath(){
+      let hasAIndex = false;
+      const possiblesInitialFileIndexes = ['./', '~/'];
+
+      possiblesInitialFileIndexes.forEach(fileIndex => {
+        if(jsonFilePath.indexOf(fileIndex) === 0)
+          hasAIndex = true;
+      });
+
+      if(hasAIndex)
+        return jsonFilePath
+      else
+        return './' + jsonFilePath;
+    }
+
+    if(jsonFilePath === undefined)
+      throw new Error('No json file provided');
+
+    return {
+      jsonFilePath: formatJsonFilePath(),
+      exportName
+    }
+  }
+
+  let result;
+  const {
+    jsonFilePath,
+    exportName
+  } = getArgsProperties();
+
+  if(exportName === undefined)
+    result = require(jsonFilePath);
+  else
+    result = require(jsonFilePath)[exportName];
+
+  return result;
+}
+
 async function translateString(toTranslate){
   const escapedString = shellescape([toTranslate]);
   const request = `python translate.py ${escapedString}`;
