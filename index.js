d2ce04c5b7c1b5ac131afccde5ec86eff0f06398
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-12-05 17:44:02 -0300

    Adding function to exec a shell command

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const { exec } = require('child_process');
+
+const shellescape = require('shell-escape');
+
+function execShellCommand(cmd){
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if(stdout)
+        return resolve(stdout);
+      else
+        return reject('Failed with exec command');
+    });
+  });
+}
+
 async function main(){
 }
 main();
